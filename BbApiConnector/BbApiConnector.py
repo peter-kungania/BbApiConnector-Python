2bb980ffe1755e9a97cbcef445e523b594567c9b
Justin Stein <justein230@gmail.com>
2021-02-11 16:57:57 -0500

    Updated to support json format for config file

--- BbApiConnector/BbApiConnector.py
+++ BbApiConnector/BbApiConnector.py
@@ -1,5 +1,5 @@
 import requests
-from configparser import ConfigParser
+import json
 
 class BbApiConnector(object):
     """
@@ -8,8 +8,7 @@ class BbApiConnector(object):
     """
     def __init__(self, config_file_name):
         self.config_file_name = config_file_name
-        self._config = ConfigParser()
-        self._config.read(config_file_name)
+        self._config = json.load(open(self.config_file_name, 'r'))
 
     def get_session(self):
         session = requests.Session()
@@ -20,14 +19,13 @@ class BbApiConnector(object):
         }
         while True:
             get_result = session.get(self._config['other']['test_api_endpoint'])
-            print(get_result.status_code)
             if get_result.status_code == 401:
-                print("Status code was 401. Retrieving updated access token...")
+                print("401: Unauthorized. Retrieving updated access token...")
                 self.update_access_token()
                 session.headers.update({'Authorization': f"Bearer {self._config['tokens']['access_token']}"})
 
             elif get_result.status_code == 200:
-                print("The access token is live!")
+                print("200: The access token is live!")
                 return session
 
             else:
@@ -40,21 +38,21 @@ class BbApiConnector(object):
     def update_access_token(self):
         token_uri = f'https://oauth2.sky.blackbaud.com/token'
         headers = {
-            # 'Authorization': f'Basic {app_details.app_id}:{app_details.secret}',
             'Content-Type': 'application/x-www-form-urlencoded'
         }
         params = {
             'grant_type': 'refresh_token',
             'refresh_token': self._config['tokens']['refresh_token'],
             'preserve_refresh_token': True,
-            'client_id': self._config['app_secrets']['app_id'],
-            'client_secret': self._config['app_secrets']['app_secret']
+            'client_id': self._config['sky_app_information']['app_id'],
+            'client_secret': self._config['sky_app_information']['app_secret']
         }
         codes = requests.post(token_uri, data=params, headers=headers)
 
-        self._config.set('tokens', 'access_token', codes.json()['access_token'])
+        self._config['tokens']['access_token'] = codes.json()['access_token']
         with open(self.config_file_name, 'w') as config_file:
-            self._config.write(config_file)
+            json.dump(self._config, config_file)
             config_file.close()
         
+        print("Access token updated.")
         return codes.json()['access_token']
